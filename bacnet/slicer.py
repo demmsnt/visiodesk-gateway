713b81c3013f11e5bacb917451d769a0092ed47b
Semen <mnogono@gmail.com>
2020-05-09 22:10:49 +0500

    removed commented code blocks

--- bacnet/slicer.py
+++ bacnet/slicer.py
@@ -19,22 +19,10 @@ class BACnetSlicer:
     def __execute_app(self, args, cwd):
         cp = subprocess.run(args, stdout=PIPE, stderr=PIPE, cwd=str(cwd))
         output = cp.stdout.decode('ascii')
-        # command = " ".join([
-        #     str(path),
-        #     str(device_id),
-        #     str(object_type_code),
-        #     str(object_id),
-        #     str(field)
-        # ])
         if self.logger.isEnabledFor(logging.DEBUG):
             self.logger.debug("execute: {} cwd: {}".format(" ".join(args), cwd))
-        # process = Popen(command, stdout=PIPE, cwd=str(cwd), text=True)
-        # (output, err) = process.communicate()
-        # exit_code = process.wait()
         if self.logger.isEnabledFor(logging.DEBUG):
             self.logger.debug("bacrp output: {}".format(output))
-            # if not exit_code == 0:
-            #     self.logger.debug("exit code: {}".format(exit_code))
         return output
 
     def execute(self, read_app: str, **kwargs):
@@ -84,7 +72,6 @@ class BACnetSlicer:
             object_type = object_type.code()
         assert (type(object_type) == int)
         path = self.config["bacrpm"]
-        # path = Path(self.bacrpm_app_path)
         cwd = path.parent
         args = [
             str(path),
@@ -94,47 +81,8 @@ class BACnetSlicer:
             ",".join(fields)
         ]
         output = self.__execute_app(args, cwd)
-        # command = " ".join([
-        #     str(path),
-        #     str(device_id),
-        #     str(object_type_code),
-        #     str(object_id),
-        #     ",".join(fields)
-        # ])
-        # if self.logger.isEnabledFor(logging.DEBUG):
-        #     self.logger.debug("execute: {} cwd: {}".format(command, cwd))
-        # process = Popen(command, stdout=PIPE, cwd=str(cwd), text=True)
-        # (output, err) = process.communicate()
-        # exit_code = process.wait()
-        # if self.logger.isEnabledFor(logging.DEBUG):
-        #     self.logger.debug("bacrpm output:\n{}".format(output))
-        #     self.logger.debug("exit code: {}".format(exit_code))
         try:
             return self.parser.parse_bacrpm(output)
         except Exception as e:
             self.logger.error("Failed parse bacrpm: {}".format(e))
             self.logger.error("Failed parser: {}".format(output))
-
-    # def execute(self, device_id: int, object_type_code: int, object_id: int, fields: list):
-    #     path = Path(self.bacrpm_app_path)
-    #     cwd = path.parent
-    #     command = " ".join([
-    #         str(self.bacrpm_app_path),
-    #         str(device_id),
-    #         str(object_type_code),
-    #         str(object_id),
-    #         ",".join(fields)
-    #     ])
-    #     if self.logger.isEnabledFor(logging.DEBUG):
-    #         self.logger.debug("execute: {} cwd: {}".format(command, cwd))
-    #     process = Popen(command, stdout=PIPE, cwd=str(cwd), text=True)
-    #     (output, err) = process.communicate()
-    #     exit_code = process.wait()
-    #     if self.logger.isEnabledFor(logging.DEBUG):
-    #         self.logger.debug("bacrpm output:\n{}".format(output))
-    #         self.logger.debug("exit code: {}".format(exit_code))
-    #     try:
-    #         return self.parser.parse_bacrpm(output)
-    #     except Exception as e:
-    #         self.logger.error("Failed parse bacrpm: {}".format(e))
-    #         self.logger.error("Failed parser: {}".format(output))
