4597cb928fddb834004154f2d36d2bfd23746dc4
Semen <mnogono@gmail.com>
2020-05-10 16:49:44 +0500

    improved configuration for spend less time with testing on remote server(MRIA)

--- config/visiobas.py
+++ config/visiobas.py
@@ -1,14 +1,14 @@
 from pathlib import Path
 
-bacnet_stack_path = Path("/W/element/npp/visiodesk-gateway/test_visiobas")
-address_cache_path = bacnet_stack_path / "resource" / "address_cache"
+bacnet_stack_path = Path("bacnet-stack")
+address_cache_path = bacnet_stack_path / "address_cache"
 
 visiobas_slicer = {
     "bacrp": bacnet_stack_path / "bacrp",
     "bacrpm": bacnet_stack_path / "bacrpm"
 }
 
-visiobas_server = {
+__local = {
     'host': 'http://localhost',
     'port': 8080,
     'ssl_verify': False,
@@ -17,3 +17,15 @@ visiobas_server = {
         'pwd': '814E284BF1B02CC2BEB58A587F5FD8DA'
     }
 }
+
+__mria = {
+    'host': 'http://10.21.171.11',
+    'port': 8080,
+    'ssl_verify': False,
+    'auth': {
+        'user': 'canio',
+        'pwd': '278D1DE033A395FE9A2AA9E1D07416D8'
+    }
+}
+
+visiobas_server = __mria
